e200cb3384f3e1dc6c64b3d9f26db3eaf48ccc8d
The Anh Nguyen <ntheanh201@gmail.com>
2022-09-28 11:30:22 +0700

    fix export pagination decorator

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export * from './pagination.module';
 export * from './constants';
 export * from './pagination.entity';
 export * from './pagination.service';
+export * from './pagination.decorator';
